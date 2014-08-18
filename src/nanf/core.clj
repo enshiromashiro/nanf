2b0d25bed219895361c637b01579beea10b38c0f
subaru45 <devnull@localhost>
2014-08-18 15:52:23 +0900

    Can print nan-result looks pretty

--- src/nanf/core.clj
+++ src/nanf/core.clj
@@ -1,8 +1,10 @@
 (ns nanf.core
   (:gen-class)
+  (:require [clojure.string
+             :as str])
   (:use [clojure.java.shell
          :only [sh]]
-        [ clojure.java.io
+        [clojure.java.io
          :only [file]]
         [ui.swing
          :only [add-listener
@@ -26,11 +28,11 @@
 (def +nan-items+
   {:papers "原稿用紙枚数"
    :chars "全文字数"
-   :lines "行数"
-   :nw-num "地の文/台詞(文字数)"
+   :linum "行数"
+   :nw-num "地の文/台詞"
    :nw-ratio "地の文/台詞(%)"
-   :kjhko-num "漢字/ひらがな/カタカナ/その他(文字数)"
-   :kjhko-ratio "漢字/ひらがな/カタカナ/その他(%)"
+   :kjhko-num "漢字/かな/カナ/他"
+   :kjhko-ratio "漢字/かな/カナ/他(%)"
    :line-head-indent "行頭字下げ"
    :close-paren "括弧閉じ前に句読点"
    :!?blank "！？後に全角ペース"
@@ -54,32 +56,86 @@
   (apply sh `(~(.getAbsolutePath (file (executable-name +nan-bin+)))
               ~fpath "-s")))
 
+(defn parse-cl-sexp [s]
+  "((:sym1 (a . b)) (sym2 42) => {:sym1 [[a b]] sym2 42}"
+  (let [sexp (read-string (str/replace s
+                                       #"\((\d+) \. (\d+)\)"
+                                       "[$1 $2]"))]
+    (letfn [(eq-first [l i] (= (first l) i))]
+      (apply merge (for [lis sexp] (assoc nil (first lis)
+                                          (vec (rest lis))))))))
+
+(defn output-str [m]
+  (letfn [(str-item [k & v]
+            (apply str `(~(+nan-items+ k) ": " ~@v "\n")))
+          (perc [a b] (float (* (/ a b) 100)))
+          (strf [f] (format "%.1f" f))]
+    (let [cnum (first (:CHAR-NUM m))
+          words (first (:WORDS-NUM m))
+          nar (- cnum words)
+          kanji (nth (:CHAR-NUM m) 1)
+          hiragana (nth (:CHAR-NUM m) 2)
+          katakana (nth (:CHAR-NUM m) 3)
+          others (nth (:CHAR-NUM m) 4)]
+      (str (str-item :papers
+                     (float (first (:PAPER-NUM  m))))
+           (str-item :chars cnum)
+           (str-item :linum
+                     (first (:LINUM m)))
+           (str-item :nw-num nar " / " words)
+           (str-item :nw-ratio (strf (perc nar cnum)) " / "
+                     (strf (perc words cnum)))
+           (str-item :kjhko-num
+                     kanji " / "
+                     hiragana " / "
+                     katakana " / "
+                     others)
+           (str-item :kjhko-ratio
+                     (strf (perc kanji cnum)) " / "
+                     (strf (perc hiragana cnum)) " / "
+                     (strf (perc katakana cnum)) " / "
+                     (strf (perc others cnum)))
+           (str-item :line-head-indent
+                     (count (:LINE-HEAD-INDENT m)))
+           (str-item :close-paren
+                     (count (:CLOSE-PAREN-WITHOUT-PUNC m)))
+           (str-item :!?blank
+                     (count (:!?-WITH-BLANK m)))
+           (str-item :ellipsis
+                     (count (:ELLIPSIS-TWICE m)))
+           (str-item :dash
+                     (count (:DASH-TWICE m)))))))
+
 (defn error-msg [& strs]
   (apply str (concat (:error +window-text+) strs)))
+
+(defn analyze [fpath]
+  (try 
+    (let [ret (run-nan fpath)]
+      (if (and (empty? (:err ret)) (zero? (:exit ret)))
+        (str "\n" fpath "\n"
+             (output-str (parse-cl-sexp (:out ret))))
+        (error-msg (str "エラーコード: "
+                        (:exit ret)
+                        "\nエラーメッセージ: "
+                        (:err ret)))))
+    (catch java.io.IOException e
+      (error-msg (str (:error +window-text+)
+                      (.getMessage e) "\n"
+                      (:no-nan +window-text+))))))
+
 
 (defn make-open-clicked [path frame]
   #(.setText path (show-open-chooser frame
                                      "text file (*.txt)"
                                      ["txt"])))
 
-
 (defn make-run-clicked [path result]
   (letfn [(append [s] (.append result s))]
     #(let [p (.getText path)]
        (if (or (nil? p) (empty? p))
          (append (error-msg (:no-file +window-text+)))
-         (try 
-           (let [ret (run-nan p)]
-             (if (and (empty? (:err ret)) (zero? (:exit ret)))
-               (append (str "\n" (:out ret)))
-               (append (error-msg "エラーコード: "
-                                  (:exit ret)
-                                  "\nエラーメッセージ: "
-                                  (:err ret)))))
-           (catch java.io.IOException e
-             (append (error-msg (:error +window-text+)
-                                (.getMessage e) "\n"
-                                (:no-nan +window-text+)))))))))
+         (append (analyze p))))))
 
 
 (defn -main [& args]
